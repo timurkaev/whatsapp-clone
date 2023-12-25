4282d6b07ef6a39d5e608b2b20bb4d44140b9f49
timurkaev <timurkaev.is@gmail.com>
2023-12-25 23:14:09 +0300

    init

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+	return (
+		<div className="bg-red-100 bg-[url(https://placekitten.com/1400)] ">
+				<h1>Login page</h1>
+		</div>
+	);
+};
+
+export default Page;
